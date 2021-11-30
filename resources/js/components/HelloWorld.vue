580317869c9731ed79002ac6bc630269cbd9b218
tukki <tukki0210@gmail.com>
2021-11-30 11:30:54 +0900

    init

---
+++ resources/js/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ data.title }}
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+
+export default {
+  name: "HelloWorld",
+  setup() {
+    const data = reactive({
+      title: "Vueeeee",
+    });
+    return {
+      data,
+    };
+  },
+};
+</script>
